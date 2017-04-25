133a03aa606c2a689a2e28d6732898590e4fe428
AmilaZimic <amila_zimic@hotmail.com>
2017-04-25 18:44:17 +0200

    Weather App C#

---
+++ WeatherApp.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace Weather_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(26, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(329, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(26, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(329, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "label5";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 405);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(3, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "label8";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(3, 324);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "label6";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(99, 50);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label7, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label6, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.label5, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label4, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(26, 235);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.5F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(480, 486);
+            this.tableLayoutPanel1.TabIndex = 13;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.label8, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(29, 96);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(162, 41);
+            this.tableLayoutPanel2.TabIndex = 14;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(26, 168);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(105, 61);
+            this.tableLayoutPanel3.TabIndex = 15;
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 1;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(137, 185);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(84, 27);
+            this.tableLayoutPanel4.TabIndex = 16;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(197, 96);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(156, 66);
+            this.pictureBox2.TabIndex = 17;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(337, 168);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1133, 539);
+            this.panel1.TabIndex = 18;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1133, 539);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Weather_App.Properties.Resources.sky2;
+            this.ClientSize = new System.Drawing.Size(1496, 719);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.tableLayoutPanel4);
+            this.Controls.Add(this.tableLayoutPanel3);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ WeatherApp.cs
@@ -0,0 +1,165 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weather_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "Please enter city";
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            button1.Click += button1_Click;
+            label1.BackColor = Color.Transparent;
+            label1.Visible = false;
+            label2.BackColor = Color.Transparent;
+            label2.Visible = false;
+            label3.BackColor = Color.Transparent;
+            label3.Visible = false;
+            label4.BackColor = Color.Transparent;
+            label4.Visible = false;
+            label5.BackColor = Color.Transparent;
+            label5.Visible = false;
+            label6.BackColor = Color.Transparent;
+            label6.Visible = false;
+            label7.BackColor = Color.Transparent;
+            label7.Visible = false;
+            label8.BackColor = Color.Transparent;
+            label8.Visible = false;
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Visible = false;
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.Visible = false;
+            //richTextBox1.Text = string.Format("{0}°C", degrees);
+            tableLayoutPanel1.BackColor = Color.Transparent;
+            tableLayoutPanel2.BackColor = Color.Transparent;
+            tableLayoutPanel3.BackColor = Color.Transparent;
+            tableLayoutPanel4.BackColor = Color.Transparent;
+            panel1.BackColor = Color.Transparent;
+            webBrowser1.Visible = false;
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                textBox1.ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string city = textBox1.Text;
+
+            string url1 = "http://api.openweathermap.org/data/2.5/weather?q=" + city + " &appid=1fbff9ce38ef2e2e896b7b80c83507c7&units=metric";
+            //instad of sample type api + instead of their id type your id + &units=metric to get temperature in Celusius
+            //example: http://api.openweathermap.org/data/2.5/weather?q=sarajevo&appid=1fbff9ce38ef2e2e896b7b80c83507c7&units=metric
+
+            WebClient wc = new WebClient();
+            string mystring1 = wc.DownloadString(url1);                       //Download whole json file
+            JObject json = JObject.Parse(mystring1);                          //Take json object
+
+            var item1 = json["main"]["temp"];
+            label1.Text = (string)item1;                                      //Display temperature when button is pressed            
+            label1.Visible = true;
+
+            var item2 = json["weather"][0]["description"];      
+            label2.Text = "Condition           " + (string)item2;             //Display description when button is pressed
+            label2.Visible = true;
+
+            var item3 = json["main"]["pressure"];             
+            label3.Text = "Pressure           " + (string)item3;              //Display pressure when button is pressed
+            label3.Visible = true;
+
+            var item4 = json["main"]["humidity"];          
+            label4.Text = "Humidity            " + (string)item4;             //Display humidity when button is pressed
+            label4.Visible = true;
+
+            var item5 = json["wind"]["speed"];          
+            label5.Text = "Wind Speed     " + (string)item5;                  //Display wind speed when button is pressed
+            label5.Visible = true;
+
+            var item6 = json["weather"][0]["icon"];                           //Take icon id from json
+            string url2 = "http://openweathermap.org/img/w/" + item6 + ".png";
+            pictureBox1.ImageLocation = url2;                                 //Display weather icon when button is pressed
+            pictureBox1.Visible = true;
+
+            Type type = typeof(DateTime);
+
+            var item7 = json["sys"]["sunrise"];
+            var result1 = item7.ToObject<double>();
+            var epoch1 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            epoch1 = epoch1.AddSeconds(result1);
+
+            label6.Text = "Sunrise            " + epoch1;                      //Display sunrise time when button is pressed
+            label6.Visible = true;
+
+            var item8 = json["sys"]["sunset"];
+            var result2 = item8.ToObject<double>();
+            var epoch2 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            epoch2 = epoch2.AddSeconds(result2);
+
+            label7.Text = "Sunset            " + epoch2;                       //Display sunset time when button is pressed
+            label7.Visible = true;
+
+            var item9 = json["name"];
+            label8.Text = (string)item9;                                      //Display name of the city when button is pressed
+            label8.Visible = true;
+
+            var item10 = json["sys"]["country"];                              //Take country id from json
+            string url3 = "https://flagpedia.net/data/flags/normal/" + item10 + ".png";
+            string urllower = url3.ToLower();                                 //item10 has to be in lower letter
+            pictureBox2.ImageLocation = urllower;                             //Display flag when button is pressed
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;           //Fit image to picture box
+            pictureBox2.Visible = true;
+
+            webBrowser1.Visible = true;
+
+            //My code for google map:
+            //string url4 = "http://maps.google.com/maps?q=" + city;
+            //webBrowser1.Navigate(city.ToString());  
+                      
+            //Code from YT tutorial
+            //https://www.youtube.com/watch?v=lRoAjs8RwfE
+
+              try
+             {
+                 StringBuilder quearryaddress = new StringBuilder();
+                 quearryaddress.Append("http://maps.google.com/maps?q=");
+
+                 if(city != string.Empty)
+                 {
+                     quearryaddress.Append(city + "," + "+");
+                 }
+
+                 webBrowser1.Navigate(quearryaddress.ToString());
+             }
+
+             catch (Exception ex)
+             {
+                 MessageBox.Show(ex.Message.ToString(), "Error");
+             }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)         //Enter instead of button
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick();
+                e.SuppressKeyPress = true;                                   //These two lines to turn of sound
+                e.Handled = true;
+            }
+        }
+    }
+}
+
